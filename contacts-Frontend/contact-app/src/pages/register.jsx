d5dfa7c1533b2ff25f1cdddfefffcd57323e2dfa
Asmit Pradhan <152243850+ASMIT123445@users.noreply.github.com>
2026-08-03 19:51:26 +0545

    Register logic in frontend added

--- contacts-Frontend/contact-app/src/pages/register.jsx
+++ contacts-Frontend/contact-app/src/pages/register.jsx
@@ -1,13 +1,54 @@
-import { use, useState } from "react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import api from "../services/api";
+import { Link, useNavigate } from "react-router-dom";
 
 function Register() {
+
+    const navigate = useNavigate();
 
     const [formData, setFormData] = useState({
         username: "",
         email: "",
         password: ""
     });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name] : e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!formData.username || !formData.email || !formData.password) {
+            showMessage("Please fill all the fields.");
+            return
+        }
+
+        try {
+            await api.post("users/register");
+
+            showMessage("Registeration successfull.");
+
+            setFormData({
+                name:"",
+                email:"",
+                password:""
+            })
+
+            setTimeout=(() => {
+                navigate("/")
+            }, 1000);
+
+            
+        } catch (error) {
+            showMessage(error.message?.data?.message || "Registration failed");
+            
+        }
+    }
+    
 
     const [message, setMessage] = useState("");
 
@@ -24,19 +65,36 @@ function Register() {
         <>
             <h1>Register user</h1>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label>UserName:</label>
                     <br/>
-                    <input type="string" placeholder="Enter your name"/>
+                    <input 
+                    type="text" 
+                    name="username" 
+                    placeholder="Enter your name"
+                    value={formData.username}
+                    onChange={handleChange}/>
                     <br/>
                     <label>Email</label>
                     <br/>
-                    <input type="email" placeholder="Enter your email"/>
+                    <input 
+                    type="email" 
+                    name="email" 
+                    placeholder="Enter your email"
+                    value={formData.email}
+                    onChange={handleChange}/>
                     <br/>
                     <label>Password:</label>
                     <br/>
-                    <input type="password" placeholder="Enter password"/>
+                    <input 
+                    type="password" 
+                    name="password" 
+                    placeholder="Enter your password"
+                    value={formData.password}
+                    onChange={handleChange}/>
+
+                    <button type="submit">Register</button>
                 </div>
             
             </form>
